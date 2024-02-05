9e051a1e012c259655b96d592e966362958723a1
Юрчик <88848940+Yrchik07@users.noreply.github.com>
2024-02-05 16:17:09 +0200

    'update'

--- src/js/main.js
+++ src/js/main.js
@@ -4,91 +4,93 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 const input = document.querySelector('.js-search-form');
-// const picCont = document.querySelector('.js_picture_container');
 const picCont = document.querySelector('.gallery');
 
 
 input.addEventListener('submit', e =>{
   e.preventDefault();
 
-  const name = e.target.elements.form.value;
+  const name = e.target.elements.form.value.trim();
+if(name === ''){
+  e.target.reset();
+  picCont.innerHTML = "";
+  return;
+}
 
   searchPicture(name).then(data => {
     renderPicture(data);
+    gallery.refresh();
   });
   e.target.reset();
   picCont.innerHTML = "";
 });
 
-
 function searchPicture(pixabay){
-  const BASE_URL = 'https://pixabay.com/api/';
-  const API_KEY = `?key=42108829-54920070c40067c28d8228e80`;
-  const Q = `&q=${pixabay}`;
-  // const TYPE ='&image_type=photo';
-  // const OR = '&orientation=horizontal';
-  // const RCH = '&safesearch=true';
-  const url = BASE_URL + API_KEY + Q;
-// console.log(url)
-// const options = {
-//   headers: {
-//     // image_type:'photo',
-//     // orientation:'horizontal',
-//     // safesearch:'true',
-//     'X-RateLimit-Limit':	'The maximum number of requests that the consumer is permitted to make in 30 minutes',
-//     'X-RateLimit-Remaining':	'The number of requests remaining in the current rate limit window',
-//     'X-RateLimit-Reset':	'The remaining time in seconds after which the current rate limit window resets',
-//   },
-// };
-  return fetch(url).then(res => res.json());
+  const BASE_URL = 'https://pixabay.com/api/?';
+    const PARAMS = new URLSearchParams({
+      key: '42108829-54920070c40067c28d8228e80',
+      q: `${pixabay}`,
+      image_type:'photo',
+      orientation:'horizontal',
+      safesearch: true,
+
+    });
+    const url = BASE_URL + PARAMS;
+
+  return fetch(url).then(res => res.json()).catch();
 }
 
 
-function pictureTemplate({webformatURL,largeImageURL,tags,likes,views,comments,downloads,total}) {
+
+function pictureTemplate({webformatURL,largeImageURL,tags,likes,views,comments,downloads}){
+
   return `<li class="gallery-item">
   <a class="gallery-link" href="${largeImageURL}">
-    <img
-      class="gallery-image"
-      src="${webformatURL}"
-      alt="${tags}"
-    />
-  </a>
-</li>`;
-}
-//   return `<div class="img-card card">
-//   <div class="image-container">
-//   <a class="gallery-link" href="${largeImageURL}">
-//     <img class="picture-image" src="${webformatURL}" alt="#">
-//     </a>
-//     <ul class="description">
-//       <li class="description-item">
-//         <h2>likes</h2>
-//         <h3>${likes}</h3>
-//       </li>
-//       <li class="description-item">
-//         <h2>views</h2>
-//         <h3>${views}</h3>
-//       </li>
-//       <li class="description-item">
-//         <h2>comments</h2>
-//         <h3>${comments}</h3>
-//       </li>
-//       <li class="description-item">
-//         <h2>downloads</h2>
-//         <h3>${downloads}</h3>
-//       </li>
-//     </ul>
-// </div>
-// </div>`;}
+    <img class="gallery-image" src="${webformatURL}" alt="${tags}">
+    </a>
+    <ul class="description">
+      <li class="description-item">
+        <h2>likes</h2>
+        <h3>${likes}</h3>
+      </li>
+      <li class="description-item">
+        <h2>views</h2>
+        <h3>${views}</h3>
+      </li>
+      <li class="description-item">
+        <h2>comments</h2>
+        <h3>${comments}</h3>
+      </li>
+      <li class="description-item">
+        <h2>downloads</h2>
+        <h3>${downloads}</h3>
+      </li>
+    </ul>
+</li> `; }
 
 function picturesTemplate(picture){
-  return picture.hits.map(pictureTemplate).join('');
+
+  let  pictureProm = picture.hits.map(pictureTemplate).join('');
+
+  if(pictureProm.length === 0){
+    iziToast.show({
+      message: 'Sorry, there are no images matching your search query. Please try again!',
+      messageColor: '#FFFFFF',
+      backgroundColor: '#B51B1B',
+      position: 'topRight',
+    });
+    return;
+  }else{
+    return pictureProm ;
+  }
 }
 
 function renderPicture(picture) {
   const markup = picturesTemplate(picture);
+  if(markup.length === 0)return;
   picCont.insertAdjacentHTML('beforeend', markup);
-  console.log(picture)
+console.log(markup)
 }
 
 const gallery = new SimpleLightbox('.gallery a', {captionDelay: 250, captionsData: 'alt'});
+
